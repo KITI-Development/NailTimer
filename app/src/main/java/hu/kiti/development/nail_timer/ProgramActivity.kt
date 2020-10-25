2cd411a9ae482bd181a8853efa1eed9c8516edd9
Karina Smirnow <smirnow.karina@gmail.com>
2020-10-25 17:59:47 +0100

    add layer dialog

--- app/src/main/java/hu/kiti/development/nail_timer/ProgramActivity.kt
+++ app/src/main/java/hu/kiti/development/nail_timer/ProgramActivity.kt
@@ -7,10 +7,12 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import butterknife.BindView
 import butterknife.ButterKnife
+import butterknife.OnClick
 
 class ProgramActivity : AppCompatActivity() {
 
-    @BindView(R.id.program_name_edit_text) lateinit var programNameEditText: EditText
+    @BindView(R.id.program_name_edit_text)
+    lateinit var programNameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,5 +46,10 @@ class ProgramActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    @OnClick(R.id.add_layer_button)
+    internal fun onAddLayerButtonClicked() {
+        LayerDialog().getInstance()?.show(supportFragmentManager, "layer")
     }
 }
