6543ca1fe36677f201dbb5a03c258f11fba97696
Karina Smirnow <smirnow.karina@gmail.com>
2020-10-25 16:34:49 +0100

    add main ui

---
+++ app/src/main/java/hu/kiti/development/nail_timer/ProgramActivity.kt
@@ -0,0 +1,11 @@
+package hu.kiti.development.nail_timer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProgramActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_program)
+    }
+}
